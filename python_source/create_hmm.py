b0606c137266f8f175d0d06e20ed87e47e50e178
alex <alexander.james.gamino@gmail.com>
2016-02-05 16:38:05 -0800

    added relative heading feature to the code

--- python_source/create_hmm.py
+++ python_source/create_hmm.py
@@ -4,13 +4,28 @@ import math
 import argparse
 import csv
 import pickle
-import hmm
+import numpy as np
+
+# HMM Library
+from hmmlearn import hmm
 
 # Symbol Definitions
-relative_angle = {'facing-toward'     : 00, 'facing-other' : 01                   }
-delta_location = {'closer'            : 10, 'farther'      : 11, 'stationary' : 12}
-delta_speed    = {'accelerating'      : 20, 'decelerating' : 21, 'constant'   : 22}
-delta_angle    = {'turning-toward'    : 30, 'turning-away' : 31, 'constant'   : 32}
+relative_angle         = {'facing-toward'     : 00, 'facing-other' : 01                   }
+delta_location         = {'closer'            : 10, 'farther'      : 11, 'stationary' : 12}
+delta_speed            = {'accelerating'      : 20, 'decelerating' : 21, 'constant'   : 22}
+delta_angle            = {'turning-toward'    : 30, 'turning-away' : 31, 'constant'   : 32}
+delta_relative_heading = {'increasing'        : 40, 'decreasing'   : 41, 'constant'   : 42} 
+
+# Real rel angle
+# CPA
+# TIME
+# Check out 84 and 80
+
+#cpa_distance   = {'will-collide'      : 50, 'may-collide'  : 51, 'neither'    : 52}
+
+# TODO(alex) CPA distance may help with the confusion between benign and the others
+# UPDATE CPA distance does not seem to predict anything very well, CPA distacne 
+# fluctuates wildly with all intents, with no appreant pattern, need more research
 
 # CSV Feature Indices
 ownBoat_x         = 0
@@ -32,15 +47,27 @@ otherBoat_theta   = 14
 otherBoat_dTheta  = 15
 
 timeStamp         = 16
+contact_id        = 17
 
 # Thresholds
 relativeAngleThresh = (math.pi / 12)
 distanceThresh = 1.0
 accelThresh = 0.1
 deltaAngleThresh = 0.01
+relHeadThresh = 0.001
 
 # Other controls
 dataLen = 20
+
+# Testing HMM names
+ramHMM    = 'hmm_ram'
+blockHMM  = 'hmm_block'
+herdHMM   = 'hmm_herd'
+benignHMM = 'hmm_benign'
+
+all_hmms = [ramHMM, blockHMM, herdHMM, benignHMM]
+intents = ['1', '5', '7', '3'] # ram, block, herd, benign
+all_intents = ['0', '8'] + intents # unknown + missing + intents
 
 # Kill program with error message
 def killProgramWithMessage(errorMSG):
@@ -76,7 +103,12 @@ def readInputFile(inputFileName):
     with open(inputFileName, 'r') as dataFile:
        r = csv.reader(dataFile, delimiter=',')  
        for row in r:
-           inputFeatures.append(row)
+           
+           # Check for ignored row 
+            if row[0][0] == '#':
+                continue
+
+            inputFeatures.append(row)
 
     inputFeatures = [map(float,x) for x in inputFeatures]
     return computeSymbols(inputFeatures), inputFeatures
@@ -126,6 +158,18 @@ def computeSymbols(inputFeatures):
         else:
             symbol.append(delta_angle['turning-toward'])
         
+        # determine delta relative heading 
+        relHead = getRelativeHeading(feature)
+
+        if abs(relHead) < relHeadThresh:
+            symbol.append(delta_relative_heading['constant'])
+        elif relHead > 0:
+            symbol.append(delta_relative_heading['increasing'])
+        else:
+            symbol.append(delta_relative_heading['decreasing'])
+            
+        #TODO determine CPA
+
         # add observed symbols to symbol list
         symbols.append(symbol)
 
@@ -182,6 +226,42 @@ def getAcceleration(feature):
 
     return math.sqrt(x_accel * x_accel + y_accel * y_accel)
 
+def getRelativeHeading(feature):
+
+    # a = other boat's previous location
+    a = [feature[otherBoat_x] - feature[otherBoat_vx]  , feature[otherBoat_y] - feature[otherBoat_vy]]
+    
+    # b = own boat's previous location
+    b = [feature[ownBoat_x]   - feature[ownBoat_vx]    , feature[ownBoat_y] - feature[ownBoat_vy]    ]
+
+    # c = own boat's current location
+    c = [feature[ownBoat_x]                            , feature[ownBoat_y]                          ]
+
+    # d = other boat's current location
+    d = [feature[otherBoat_x]                          , feature[otherBoat_y]                        ]
+
+    # e = our next location
+    e = [feature[ownBoat_x]   + feature[ownBoat_vx]    , feature[ownBoat_y] + feature[ownBoat_vy]    ]
+
+    # Create some unit vectors
+    ba = [a[0] - b[0], a[1] - b[1]]
+    bc = [c[0] - b[0], c[1] - b[1]]
+
+    cd = [d[0] - c[0], d[1] - c[1]]
+    ce = [e[0] - c[0], e[1] - c[1]] 
+
+    for vector in [ba, bc, cd, ce]:
+        magnitude = math.sqrt(vector[0]*vector[0] + vector[1]*vector[1])
+
+        if magnitude > 0.0:
+            vector[0] /= magnitude
+            vector[1] /= magnitude
+
+    previous_angle  = math.atan2(ba[1], ba[0]) - math.atan2(bc[1], bc[0])
+    current_angle   = math.atan2(cd[1], cd[0]) - math.atan2(ce[1], ce[0])
+
+    return current_angle - previous_angle
+
 def collapseObservations():
 
     collapsedObservations = dict()
@@ -191,8 +271,9 @@ def collapseObservations():
         for k2, v2 in delta_location.iteritems():
             for k3, v3 in delta_speed.iteritems():
                 for k4, v4 in delta_angle.iteritems():
-                    counter += 1
-                    collapsedObservations[str(v1)+str(v2)+str(v3)+str(v4)] = counter
+                    for k5, v5 in delta_relative_heading.iteritems():
+                        counter += 1
+                        collapsedObservations[str(v1)+str(v2)+str(v3)+str(v4)+str(v5)] = counter
 
     return collapsedObservations
 
@@ -210,7 +291,8 @@ def train(inputFileDir, outputFileName, numStates):
         allPossibleSymbols.append(v)
 
     # Initialize HMM
-    HMM = hmm.HMM(numStates, V=allPossibleSymbols)
+    #TODO try different parameters
+    HMM = hmm.GaussianHMM(n_components=numStates, n_iter=1000)
 
     # Get file names from training directory
     filenames = getFilesFromDir(inputFileDir)
@@ -218,21 +300,31 @@ def train(inputFileDir, outputFileName, numStates):
     # iterate over all of our training files
     for f in filenames:
 
+        # Debug print statment
+        print "Training with file: ", f
+
         # More iterations here
         observed_fixed = []
         observedSymbols, features = readInputFile(f)
 
         # Convert list of symbols to unique interger identifier
         for s in observedSymbols:
-            observed_fixed.append(collapsedObservations[str(s[0])+str(s[1])+str(s[2])+str(s[3])])
+            observed_fixed.append(collapsedObservations[str(s[0])+str(s[1])+str(s[2])+str(s[3])+str(s[4])])
 
         # Bin data
         split = binData(observed_fixed)
 
-        # Update HMM
-        HMM = hmm.baum_welch(HMM, split, epochs=20)
+        # Debug print for all observed sequences
+        #for s in split:
+        #    print s
 
-        break
+        # Update HMM
+        HMM.fit(split)
+
+    # Test for good HMM
+    # This will fail if there's a problem
+    a = np.array([28] * dataLen).reshape(1,-1)
+    p = HMM.score(a)
 
     # Save HMM to disk
     saveHmm(outputFileName, HMM) 
@@ -252,17 +344,25 @@ def saveHmm(outputFileName, hmm) :
     with open(outputFileName, 'w+') as f:
         pickle.dump(hmm,f)
 
-def loadHmm(inputFileName):
-    hmm = None
-    with open(inputFileName) as f:
-        hmm = pickle.load(f)
-    return hmm
+def loadHmms():
+
+    hmms = []
+
+    # Load HMMs from file
+    for filename in all_hmms:
+        try:
+            with open(filename) as f:
+                hmms.append(pickle.load(f))
+        except EnvironmentError:
+            killWithMessage('There is a problem with the input files')
+
+    return hmms
 
 # Test HMM
-def test(inputFileDir, inHmmName):
+def test(inputFileDir):
 
     # Load HMM data from file
-    HMM = loadHmm(inHmmName)
+    HMMs = loadHmms()
 
     # Get file names from training directory
     filenames = getFilesFromDir(inputFileDir)
@@ -273,6 +373,9 @@ def test(inputFileDir, inHmmName):
     # iterate over all of our training files
     for f in filenames:
 
+        # Debug print statment
+        print "Testing with file: ", f
+
         # Initialize Variables
         probs = []
         
@@ -282,15 +385,31 @@ def test(inputFileDir, inHmmName):
 
         # Convert list of symbols to unique interger identifier
         for s in observedSymbols:
-            observed_fixed.append(collapsedObservations[str(s[0])+str(s[1])+str(s[2])+str(s[3])])
+            observed_fixed.append(collapsedObservations[str(s[0])+str(s[1])+str(s[2])+str(s[3])+str(s[4])])
 
         # Bin data
         split = binData(observed_fixed)
 
         # Generate test data
         for line in split:
-            logProb, _, _ = hmm.forward(HMM, line)
-            probs.append(math.exp(logProb))
+
+            scores = []
+            prob = []
+            totalSum = 0
+
+            # Test against all available hmms
+            for HMM in HMMs:
+                scores.append(HMM.score(np.array(line).reshape(1,-1)))
+
+            # Figure out the scaling factor
+            for score in scores:
+                totalSum += math.exp(score)
+
+            # Determine probability for each hmm class
+            for score in scores:
+                prob.append(math.exp(score) / totalSum)
+
+            probs.append(prob)
 
         # Save to file:
         createOutputReport(f, probs, features)
@@ -300,31 +419,98 @@ def createOutputReport(filename, probs, symbols):
     # Initialize Variables
     header = "timestamp, source_id, contact_id, contact_type, intent_type, group_id, intent_class, intent_prob, group_confidence\n"
     index = dataLen
+    originalIndex = 0
+    intentNum = 0
+    skipRow = False
     
     # Get save location
     parentDir =  os.path.dirname(filename)
 
-    # Begin writting report
-    with open(parentDir + '/probability_report.csv', 'w+') as f:
+    # Also open original file
+    with open(filename, 'r') as originalFile:
 
-        f.write(header)
+        # Open as a .csv
+        tempCSV = csv.reader(originalFile, delimiter=',')  
+        originalCSV = []
+        for row in tempCSV:
+            originalCSV.append(row)
 
-        for p in probs:
-
-            # Timestamp
-            f.write(str(symbols[index][timeStamp]) + ',')
+        # Begin writting report
+        with open(parentDir + '/probability_report.csv', 'w+') as f:
             
-            # Some constants
-            f.write("1,4,99,1,1,1,") 
+            # Output header
+            f.write(header)  
 
-            # Probability
-            f.write(str(p) + ',')
-            
-            # More constants
-            f.write("1\n")
+            # ID rows where there isn't enough data
+            while originalCSV[originalIndex][0][0] == '#':
 
-            # Increase index
-            index += 1
+                # Output placeholder record
+                for intent_index in all_intents:
+
+                    intent_probability = '0'
+                    if intent_index == '8':
+                        intent_probability = '1' # we consider this special case 'unknown'
+
+                    f.write(originalCSV[originalIndex][1] + ",1,99,99,0,99," + intent_index + ',' + intent_probability + ",0\n")
+
+                originalIndex += 1
+
+            # Then on to rows that didn't have enough history
+            for i in range(0, dataLen):
+
+                # Output placeholder record
+                for intent_index in all_intents:
+
+                    intent_probability = '0'
+                    if intent_index == '8':
+                        intent_probability = '1' # we consider this special case 'unknown'
+                    
+                    # This need to be updated to include an unknown data tag
+                    f.write(str(originalCSV[originalIndex][-2]) + ",1,99,99,0,99," + intent_index + ',' + intent_probability + ",0\n")
+
+                originalIndex += 1
+
+            #TODO(alex): There needs to be logic here for dealing with missing/unknown intents
+            #            Unknown data being close to an even probability across all categories.
+
+            for prob in probs:
+
+                # Take care of the unknown and missing here 
+                f.write(str(symbols[index][timeStamp]) + ",1,99,99,0,99,0,0,0\n")
+                f.write(str(symbols[index][timeStamp]) + ",1,99,99,0,99,8,0,0\n")
+
+                # This need to be updated to include an unknown data tag
+
+                for p in prob:
+                    # Timestamp
+                    f.write(str(symbols[index][timeStamp]) + ',')
+                    
+                    # Some constants
+                    f.write("1,")
+
+                    # Contact ID + more constants
+                    f.write(str(symbols[index][contact_id]))
+                    f.write(",99,0,99,")
+                    
+                    # Intent type
+                    f.write(intents[intentNum])
+
+                    # Constants
+                    f.write(",") 
+
+                    # Probability
+                    f.write(str(p) + ',')
+
+                    # More constants
+                    f.write("0\n")
+
+                    # Increast intent number
+                    intentNum += 1
+                    intentNum %= len(intents)
+
+                # Increase index
+                originalIndex += 1
+                index += 1
 
 # Main
 if __name__ == '__main__':
@@ -333,24 +519,29 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test or train HMM')
     parser.add_argument('input_file_dir', help='The .csv input file directory for training/testing data')
     parser.add_argument('mode', help='Either "train" or "test"')
-    parser.add_argument('hmm', help='Name of HMM to be trained or tested')
+    parser.add_argument('--outName', help='Name of HMM to be trained')
     parser.add_argument('--numStates', help='The desired number of hidden states')
     args = parser.parse_args()
 
     # Test with input files, and produce output hmm
     if args.mode == 'train':
-        numStates = 2 
+        numStates = 5 
+        outName = 'hmm_defaultName'
 
         if args.numStates and args.numStates in rang(2,6):
-            numStates = int(numStates)
+            numStates = int(args.numStates)
 
-        train(args.input_file_dir, args.hmm, numStates) 
+        if args.outName:
+            outName = args.outName
+
+        train(args.input_file_dir, outName, numStates) 
 
     # Train with input files, and product output probabilities
     elif args.mode == 'test':
-        test(args.input_file_dir, args.hmm) 
+        test(args.input_file_dir) 
 
     # Something has gone wrong
     else:
         killProgramWithMessage('Argument Error: Invalid Mode')
+
 
